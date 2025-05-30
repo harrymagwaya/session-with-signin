09a9b00d56779501a5c94a41850cd6096603c9ab
mwijuka <mwijukaharoid@gmail.com>
2025-05-30 16:18:00 +0300

    added views

--- src/main/java/com/signin/signin/controllers/UserController.java
+++ src/main/java/com/signin/signin/controllers/UserController.java
@@ -1,5 +1,51 @@
 package com.signin.signin.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import com.signin.signin.repository.UserRepository;
+import com.signin.signin.views.SignInView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@Controller
 public class UserController {
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private SignInView signInView
+
+    public UserController(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/users/view")
+    public SignInView getAllUserz(Model model) {
+        return signInView;
+    }
+
+    @PostMapping("path")
+    public String addUser(@RequestParam Map<String, String> newuser, HttpServletResponse response) {
+        //TODO: process POST request
+        
+        return "";
+    }
     
+
+    
+
+
+
+
 }
---
+++ src/main/java/com/signin/signin/views/SignInView.java
@@ -0,0 +1,17 @@
+package com.signin.signin.views;
+
+import com.signin.signin.controllers.UserController;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+
+public class SignInView extends VerticalLayout {
+
+    private UserController controller;
+    public SignIn(UserController controller){
+        this.controller = controller;
+    }
+
+
+}
